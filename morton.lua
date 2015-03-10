7b9e6af21bb45074e923c80e7241f96d04b571a9
Steven Johnson <steve@xibalbastudios.com>
2015-03-10 16:16:48 -0600

    Added pairs functions to Morton numbers, plus member accessors and updaters to both two- and three-element numbers

--- morton.lua
+++ morton.lua
@@ -37,12 +37,49 @@ local M = {}
 
 --
 local function AuxPair (mnum)
-	-- STUFF
+	mnum = band(0x55555555, mnum)
+	mnum = band(0x33333333, bor(mnum, rshift(mnum, 1)))
+	mnum = band(0x0F0F0F0F, bor(mnum, rshift(mnum, 2)))
+	mnum = band(0x00FF00FF, bor(mnum, rshift(mnum, 4)))
+	mnum = band(0x0000FFFF, bor(mnum, rshift(mnum, 8))) -- 0xFFFF = 65535, i.e. mask of low 16 bits
+
+	return mnum
 end
 
 --- DOCME
 function M.MortonPair (mnum)
-	-- TODO!
+	return AuxPair(mnum), AuxPair(rshift(mnum, 1))
+end
+
+--- DOCME
+function M.MortonPair_X (mnum)
+	return AuxPair(mnum)
+end
+
+--- DOCME
+function M.MortonPair_Y (mnum)
+	return AuxPair(rshift(mnum, 1))
+end
+
+-- Helper to prepare a component for a Morton pair
+-- The right-hand side comments show how the shifts and masks spread a given 16-bit number across 32 bits 
+local function AuxMorton2 (x)
+	x = band(0x00FF00FF, bor(x, lshift(x, 8))) -- 0000 0000 1111 1111 0000 0000 1111 1111
+	x = band(0x0F0F0F0F, bor(x, lshift(x, 4))) -- 0000 1111 0000 1111 0000 1111 0000 1111
+	x = band(0x33333333, bor(x, lshift(x, 2))) -- 0011 0011 0011 0011 0011 0011 0011 0011
+	x = band(0x55555555, bor(x, lshift(x, 1))) -- 0101 0101 0101 0101 0101 0101 0101 0101
+
+	return x
+end
+
+--- DOCME
+function M.MortonPairUpdate_X (mnum, x)
+	return band(0xAAAAAAAA, mnum) + AuxMorton2(x) -- 1010 1010 1010 1010 1010 1010 1010 1010
+end
+
+--- DOCME
+function M.MortonPairUpdate_Y (mnum, y)
+	return band(0x55555555, mnum) + lshift(AuxMorton2(y), 1) -- 0101 0101 0101 0101 0101 0101 0101 0101
 end
 
 -- Helper to extract a component from a Morton triple
@@ -67,14 +104,19 @@ function M.MortonTriple (mnum)
 	return AuxTriple(lshift(mnum, 2)), AuxTriple(lshift(mnum, 1)), AuxTriple(mnum)
 end
 
---
-local function AuxMorton2 (x)
-	-- STUFF
+--- DOCME
+function M.MortonTriple_X (mnum)
+	return AuxTriple(lshift(mnum, 2))
 end
 
 --- DOCME
-function M.Morton2 (x, y)
-	-- TODO!
+function M.MortonTriple_Y (mnum)
+	return AuxTriple(lshift(mnum, 1))
+end
+
+--- DOCME
+function M.MortonTriple_Z (mnum)
+	return AuxTriple(mnum)
 end
 
 -- Helper to prepare a component for a Morton triple
@@ -86,6 +128,26 @@ local function AuxMorton3 (x)
 	x = band(0x24924924, bor(x, lshift(x, 2)))  -- 100 100 100 100 100 100 100 100 100 100
 
 	return x
+end
+
+--- DOCME
+function M.MortonTripleUpdate_X (mnum, x)
+	return band(0x36DB6DB6, mnum) + rshift(AuxMorton3(x), 2) -- 110 110 110 110 110 110 110 110 110 110
+end
+
+--- DOCME
+function M.MortonTripleUpdate_Y (mnum, y)
+	return band(0x2DB6DB6D, mnum) + rshift(AuxMorton3(y), 1) -- 101 101 101 101 101 101 101 101 101 101
+end
+
+--- DOCME
+function M.MortonTripleUpdate_Z (mnum, z)
+	return band(0x1B6DB6DB, mnum) + AuxMorton3(z) -- 011 011 011 011 011 011 011 011 011 011
+end
+
+--- DOCME
+function M.Morton2 (x, y)
+	return AuxMorton2(x) + lshift(AuxMorton2(y), 1)
 end
 
 --- Builds a Morton number out of three parts.
